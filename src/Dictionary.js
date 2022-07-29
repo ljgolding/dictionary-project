bb867adc811308942f19bd2ea4440a429d64651e
ljgolding <101936562+ljgolding@users.noreply.github.com>
2022-07-29 10:23:57 -0600

    Add handleResponse function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,6 +7,10 @@ export default function Dictionary () {
 
     let [keyWord, setKeyWord] = useState("");
 
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
+
     function search (event) {
         event.preventDefault();
         alert(`searching for ${keyWord} definition`);
