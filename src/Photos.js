ef70805838ff7d892eab0647c055eaa1a8bb9650
ljgolding <101936562+ljgolding@users.noreply.github.com>
2022-08-01 17:34:09 -0600

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -14,7 +14,7 @@ return (
         return (
         <div className="col-4" key={index}>
         <a href={photo.src.original} target="_blank" rel="noreferrer" >
-          <img src={photo.src.landscape} key={index} className="img-fluid" alt=""/>
+          <img src={photo.src.landscape} key={index} className="img-fluid" alt={photo.src.photographer}/>
         </a>
         </div>
         );
