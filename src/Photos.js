ffd66c434d2370d6bfbf7b2609d4959364c8ae06
ljgolding <101936562+ljgolding@users.noreply.github.com>
2022-08-01 17:31:42 -0600

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -13,8 +13,8 @@ return (
     {props.photos.map(function(photo, index) {
         return (
         <div className="col-4" key={index}>
-        <a href={photo.src.original} target="_blank" rel="noreferrer" alt="Photo">
-          <img src={photo.src.landscape} key={index} className="img-fluid" />
+        <a href={photo.src.original} target="_blank" rel="noreferrer" >
+          <img src={photo.src.landscape} key={index} className="img-fluid" alt=""/>
         </a>
         </div>
         );
